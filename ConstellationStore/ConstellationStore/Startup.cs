1e6c6328a6c29aeeb46ef873d4853f08379cd569
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-01 11:59:05 -0500

    project created on Visual Studio 2013 - 4.5.1 - ASP.NET Web Application

---
+++ ConstellationStore/ConstellationStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConstellationStore.Startup))]
+namespace ConstellationStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

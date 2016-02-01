eef45c3e8d3a6d75ea0fdec585a755d2e20eb78d
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-01 12:52:57 -0500

    added bootstrap-template-css and index page

--- ConstellationStore/ConstellationStore/App_Start/BundleConfig.cs
+++ ConstellationStore/ConstellationStore/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace ConstellationStore
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-template.css",
+                      "~/Content/ConstellationStore.css"));
         }
     }
 }

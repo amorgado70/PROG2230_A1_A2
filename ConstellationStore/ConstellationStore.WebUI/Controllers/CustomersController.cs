aa36174b86e34f4fd966247b74ed4b6eb86e5dc7
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-10 11:03:19 -0500

    CustomerController : Include IRepositoryBase + Index + Details + Create

--- ConstellationStore/ConstellationStore.WebUI/Controllers/CustomersController.cs
+++ ConstellationStore/ConstellationStore.WebUI/Controllers/CustomersController.cs
@@ -6,135 +6,63 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using ConstellationStore.Contracts.Data;
 using ConstellationStore.Models;
+using ConstellationStore.Contracts.Data;
+using ConstellationStore.Contracts.Repositories;
 
 namespace ConstellationStore.WebUI.Controllers
 {
     public class CustomersController : Controller
     {
+
+        IRepositoryBase<Customer> customers;
+
+        public CustomersController(IRepositoryBase<Customer> customers)
+        {
+            this.customers = customers;
+        }//end Constructor
+
         private DataContext db = new DataContext();
 
-        // GET: Customers
-        //public ActionResult Index()
-        //{
-        //    return View(db.Customers.ToList());
-        //}
+
+        // GET: List with filter
         public ActionResult Index(string searchString)
         {
-            var customers = from m in db.Customers
-                         select m;
+            var model = customers.GetAll();
 
             if (!String.IsNullOrEmpty(searchString))
             {
-                customers = customers.Where(s => s.CustomerName.Contains(searchString));
+                model = model.Where(s => s.CustomerName.Contains(searchString));
             }
 
-            return View(customers);
+            return View(model);
         }
 
         // GET: Customers/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
+            var model = customers.GetById(id);
+            if (model == null)
             {
                 return HttpNotFound();
             }
-            return View(customer);
+            return View(model);
         }
 
         // GET: Customers/Create
         public ActionResult Create()
         {
-            return View();
+            var model = new Customer();
+            return View(model);
         }
-
-        // POST: Customers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CustomerId,CustomerName,PictureUrl,Address1,Address2,Town,PostalCode,HomePhone,BusinessPhone,EmailAddress")] Customer customer)
+        public ActionResult Create(Customer customer)
         {
-            if (ModelState.IsValid)
-            {
-                db.Customers.Add(customer);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(customer);
-        }
-
-        // GET: Customers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
-
-        // POST: Customers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CustomerId,CustomerName,PictureUrl,Address1,Address2,Town,PostalCode,HomePhone,BusinessPhone,EmailAddress")] Customer customer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(customer).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(customer);
-        }
-
-        // GET: Customers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
-
-        // POST: Customers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Customer customer = db.Customers.Find(id);
-            db.Customers.Remove(customer);
-            db.SaveChanges();
+            customers.Insert(customer);
+            customers.Commit();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
